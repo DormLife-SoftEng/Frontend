3f696c4c4ece468f592fed092f8d1bfe4b6c795f
Poon4270 <Poon4270@gmail.com>
2020-10-26 14:25:42 +0700

    Reform Dropzone

--- src/components/dormown/Dropzonef.tsx
+++ src/components/dormown/Dropzonef.tsx
@@ -1,24 +1,40 @@
-import React, {Component} from 'react'
-import {DropzoneArea} from 'material-ui-dropzone'
+import React, {Component, useState} from 'react'
+import {DropzoneAreaBase,DropzoneDialog} from 'material-ui-dropzone'
 import { AnyARecord } from 'dns';
-
+import { withStyles, createStyles, Button } from "@material-ui/core";
+import styles from'./dropzoneclass.module.css';
 interface DropzoneProps {
     files : any
     setFiles : any
 }
-
 const Dropzonef = (props : DropzoneProps) => {
+    const [open, setOpen] = useState<boolean>(false);
     const {files,setFiles} = props
     const handleChange = (files : any) => {
         setFiles(files)
+        setOpen(false)
     }
     return (
-        <DropzoneArea
-            onChange={handleChange}
-            acceptedFiles ={['image/jpeg', 'image/png', 'image/bmp','doc/pdf']}
-            dropzoneText = "Drop or click here"
-        />
+        <div>
+        <Button variant="contained" color="primary" onClick={() => setOpen(true)}>
+        Add Image
+        </Button>
+
+        <DropzoneDialog
+        acceptedFiles={['image/*']}
+        cancelButtonText={"cancel"}
+        submitButtonText={"submit"}
+        maxFileSize={5000000}
+        open={open}
+        onClose={() => setOpen(false)}
+        onSave={
+          handleChange
+        }
+        showPreviews={true}
+        showFileNamesInPreview={true}
+      />
+      </div>
     )
 }
 
-export default Dropzonef
+export default (Dropzonef)
