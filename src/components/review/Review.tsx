6c3484584b0c39f45b24ca47111f83edd2d94d2c
poom1g <phatthrawat.w@ku.th>
2020-10-26 16:33:01 +0700

    Review Add Page

--- src/components/review/Review.tsx
+++ src/components/review/Review.tsx
@@ -3,21 +3,24 @@ import { withFormik } from "formik";
 import * as Yup from "yup";
 import { withStyles, createStyles } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
-import { Navbar, Nav, Row, Col, Button, Container } from "react-bootstrap";
+import { Navbar, Nav, Row, Button, Col, Container } from "react-bootstrap";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import Rating from '@material-ui/lab/Rating';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import TextField from "@material-ui/core/TextField";
 import FormControl from "@material-ui/core/FormControl";
+import Dropzonef from "./Dropzonef"
 
-function ReviewAdd() {
+function ReviewAdd(props: any) {
   const [value, setValue] = React.useState<number | null>(0);
   const history = useHistory();
+  const [ReviewImage, setFiles] = useState<any[]>([])
+
   return (
     <div>
       <div>
-        <Navbar style={{ padding: "1%" ,backgroundColor: "#F55E61"}}>
+        <Navbar style={{ padding: "1%", backgroundColor: "#F55E61" }}>
           <Nav className="text-right">
             <Button variant="" onClick={() => history.goBack()}>
               <ArrowBackIosIcon htmlColor="white" fontSize="large" />
@@ -38,29 +41,55 @@ function ReviewAdd() {
           </Nav>
         </Navbar>
       </div>
-      <div style={{padding:"3% 3%"}}>
-      <Box component="fieldset" mb={3} borderColor="transparent">
-        <Typography component="legend" style={{fontSize:"40px"}}>Rating</Typography>
-        <br />
-        <Rating
-          name="simple-controlled"
-          size="large"
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-            alert(JSON.stringify(newValue));
-          }}
-        />
-      </Box>
-      <FormControl component="fieldset" style={{ float: "left", backgroundColor: "#E7E7E7", width:"650px", height:"222px"}}>
-        <TextField
-          placeholder="Type something..."
-          style={{ width: "100%", height: "100%" }}
-          multiline
-          variant="filled"
-          rows={10}
-        />
-      </FormControl>
+      <div style={{ padding: "3% 3%" }}>
+        <Box component="fieldset" mb={3} borderColor="transparent">
+          <Typography component="legend" style={{ fontSize: "40px" }}>Rating</Typography>
+          <br />
+          <Rating
+            name="simple-controlled"
+            size="large"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+              alert(JSON.stringify(newValue));
+            }}
+          />
+        </Box>
+        <Box component="fieldset" mb={3} borderColor="transparent">
+          <FormControl component="fieldset" style={{ float: "left", backgroundColor: "#E7E7E7", width: "650px", height: "222px" }}>
+            <TextField
+              placeholder="Type something..."
+              style={{ width: "100%", height: "100%" }}
+              multiline
+              variant="filled"
+              rows={10}
+            />
+          </FormControl>
+        </Box>
+        < br />
+        <Box component="fieldset" mb={3} borderColor="transparent">
+          <Dropzonef files={ReviewImage} setFiles={(files: any) => { setFiles(files); }} />
+        </Box>
+        <div style={{ padding: "5% 5%", height: "10px" }}>
+          <Row>
+            <Col lg={1}>
+              <Button
+                variant="contained"
+                style={{ backgroundColor: "#F55E61", color: "white" }}
+              >
+                SUBMIT
+                </Button>
+            </Col>
+            <Col  lg={1}>
+              <Button
+                variant="contained"
+                style={{ backgroundColor: "#989595", color: "white" }}
+              >
+                CANCEL
+                </Button>
+            </Col>
+          </Row>
+        </div>
       </div>
     </div>
   )
--- src/components/routes/DormOwnerRoute.tsx
+++ src/components/routes/DormOwnerRoute.tsx
@@ -7,8 +7,8 @@ const DormOwnerRoute = (props : RouteProp) => {
     const {authToken}  : authContextType  = useAuth()
     const {path , Component} = props
     const routeComponent : any = () => {
-        //return authToken && authToken.role === "owner" ? <Component /> : <Redirect to="/signin" />
-        return <Component />
+        return authToken && authToken.role === "owner" ? <Component /> : <Redirect to="/signin" />
+        //return <Component />
     }
     return <Route component={routeComponent} exact path={path} />
 }
