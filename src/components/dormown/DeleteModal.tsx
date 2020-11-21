3515dfdec9b9f19ed5203e2aa27c13341fd4d0b1
025528078pP <peachzxzx@hotmail.com>
2020-11-21 16:45:08 +0700

    Change modal to responsive

--- src/components/dormown/DeleteModal.tsx
+++ src/components/dormown/DeleteModal.tsx
@@ -2,15 +2,7 @@
 import React, { useState } from "react";
 import "./style.css";
 
-import {
-  Image,
-  Row,
-  Col,
-  Modal,
-  Button,
-  Card,
-  Container,
-} from "react-bootstrap";
+import { Image, Row, Col, Modal, Button, Card, Container } from "react-bootstrap";
 import dormownerService, { Ticket } from "../../services/dormowner.service";
 import dorminfoService from "../../services/dorminfo.service";
 import { useHistory } from "react-router-dom";
@@ -50,12 +42,7 @@ function DeleteModal(props: any) {
       >
         Delete Dorm
       </Button>
-      <Modal
-        centered={true}
-        show={show}
-        onHide={handleClose}
-        dialogClassName="delete"
-      >
+      <Modal centered={true} show={show} onHide={handleClose} dialogClassName="delete">
         <Modal.Body
           style={{
             fontSize: "1rem",
@@ -63,50 +50,51 @@ function DeleteModal(props: any) {
             background: "#F55E61",
           }}
         >
-          <Container style={{}}>
-            <Row
-              style={{
-                width: "100%",
-              }}
-            >
+          <Row>
+            <Col xs={0}></Col>
+            <Col xs={12}>
               <h1
                 style={{
                   textAlign: "center",
                   fontSize: "40px",
+                  color: "#FFFFFF",
                 }}
               >
                 Are you sure to delete this dorm?
               </h1>
-            </Row>
-            <Row>
-              <Col lg={6} style={{ textAlign: "center" }}>
-                <Button
-                  onClick={() => deleteSubmit()}
-                  style={{
-                    borderRadius: "10px",
-                    background: "white",
-                    color: "#F55E61",
-                    fontWeight: "bold",
-                  }}
-                >
-                  Confirm
-                </Button>
-              </Col>
-              <Col lg={6} style={{ textAlign: "center" }}>
-                <Button
-                  onClick={handleClose}
-                  style={{
-                    borderRadius: "10px",
-                    background: "white",
-                    color: "#F55E61",
-                    fontWeight: "bold",
-                  }}
-                >
-                  Cancel
-                </Button>
-              </Col>
-            </Row>
-          </Container>
+            </Col>
+            <Col xs={0}></Col>
+          </Row>
+          <Row>
+            <Col xs={2}></Col>
+            <Col xs={4} style={{ textAlign: "center" }}>
+              <Button
+                onClick={() => deleteSubmit()}
+                style={{
+                  borderRadius: "10px",
+                  background: "white",
+                  color: "#F55E61",
+                  fontWeight: "bold",
+                }}
+              >
+                Confirm
+              </Button>
+            </Col>
+            <Col xs={4} style={{ textAlign: "center" }}>
+              <Button
+                onClick={handleClose}
+                style={{
+                  borderRadius: "10px",
+                  background: "white",
+                  color: "#F55E61",
+                  fontWeight: "bold",
+                }}
+              >
+                Cancel
+              </Button>
+            </Col>
+            <Col xs={2}></Col>
+          </Row>
         </Modal.Body>
       </Modal>
     </>
