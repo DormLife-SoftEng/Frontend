859e9d2902dfa3c94769c51de1c7508bb4adb4a8
poon4270 <42200108+poon4270@users.noreply.github.com>
2020-11-23 01:12:28 +0700

    fixing conflict

--- src/services/dorminfo.service.ts
+++ src/services/dorminfo.service.ts
@@ -1,37 +1,4 @@
 import axios from "axios";
-<<<<<<< HEAD
-import { tokenDto } from "../components/type";
-const DORM_URL = "http://localhost:5000/api/v1/dorms"
-const REV_URL = "http://localhost:5000/api/v1/reviews"
-
-async function getOneDorm(dormID : string) {
-    const result = await axios.get(`${DORM_URL}/${dormID}`)
-    console.log(result.data)
-    return result.data
-}
-
-async function getDormReviews(dormID : string) {
-    const token = localStorage.getItem("token")
-    if (token) {
-        const tokenObj : tokenDto = JSON.parse(token)
-        const access_token = tokenObj.access_token
-        const config = {
-            headers: { Authorization : `Bearer ${access_token}`},
-            params:{'dormId': dormID},
-        }
-    try{
-    const result = await axios.get(`${REV_URL}`, config)
-    return result.data
-    } catch(err) {
-        return {}
-    }
-}
-}
-export default {
-    getOneDorm,
-    getDormReviews
-}
-=======
 const DORM_URL = `http://${process.env.REACT_APP_BACKEND_BASE_URL}:${process.env.REACT_APP_BACKEND_URL_PORT}/api/v1/dorms`;
 const REV_URL = `http://${process.env.REACT_APP_BACKEND_BASE_URL}:${process.env.REACT_APP_BACKEND_URL_PORT}/api/v1/reviews`;
 
@@ -54,4 +21,3 @@ export default {
   getOneDorm,
   getDormReviews,
 };
->>>>>>> 1fd91495e44e3607a67a2eb4faf7fb5f0f6d75c4
